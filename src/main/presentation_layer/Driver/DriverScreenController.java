1aa2745cf165a1672750a297e3a8489e4ac04e48
andrewoc98 <60736542+andrewoc98@users.noreply.github.com>
2020-12-13 18:56:46 +0000

    Update DriverScreenController.java

--- src/main/presentation_layer/Driver/DriverScreenController.java
+++ src/main/presentation_layer/Driver/DriverScreenController.java
@@ -1,18 +1,24 @@
 package main.presentation_layer.Driver;
 
 import javafx.fxml.FXML;
+import main.Globals;
 import main.data_layer.DatabaseRepository;
 import main.presentation_layer.PresentationLoader;
 
 import java.util.ArrayList;
+import java.util.List;
+import javax.swing.text.Document;
 
 import com.mongodb.client.FindIterable;
 
-import org.bson.Document;
 import org.bson.types.ObjectId;
 
+import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
 import javafx.scene.control.TabPane;
@@ -29,31 +35,44 @@ public class DriverScreenController {
     private VBox orders_pane;
     @FXML
     private VBox accept_pane;
-
-    DatabaseRepository db;
-
+    @FXML
+    private VBox DetailsPane;
+    @FXML
+    private HBox root;
     Button btn;
     Label lab;
     Label dashlab;
     CheckBox cbx;
-    // StackPane DetailsStack;
-    HBox root;
+    //StackPane DetailsStack;
+
     VBox OrderTab;
     VBox AcceptTab;
     VBox DetailsTab;
     // Testing
-    private int custSize;
-    private int UnAcptIdSize;
-    private ArrayList<Object> Price;
-    private ArrayList<Object> Cust;
-    private ArrayList<Object> Rest;
-    private ArrayList<String> unacptid;
-    private ArrayList<Object> acptid;
+    private static int custSize;
+    private static int UnAcptIdSize;
+    private static ArrayList<Object> Price;
+    private static ArrayList<Object> Cust;
+    private static ArrayList<Object> Rest;
+    private static ArrayList<String> unacptid;
+    private static ArrayList<Object> acptid;
 
     EventHandler<ActionEvent> handleClick = new EventHandler<ActionEvent>() {
         @Override
         public void handle(ActionEvent evt) {
-            System.out.println("Handle Click");
+            Button sourceButton = (Button) evt.getSource();
+            String id = sourceButton.getId();
+            ObjectId x = new ObjectId(id.substring(5));
+            if (id.substring(0, 5).equals("Order")) {
+                System.out.println("Order Completed: " + id.substring(5));
+                         DatabaseRepository.completeOrder(x);
+                     }
+
+                     if (id.substring(0, 5).equals("NewOr")) {
+                           System.out.println("Order Accepted: " + id.substring(8));
+               
+                           DatabaseRepository.acceptOrder(x);
+                       }
             evt.consume();
         }
     };
@@ -61,45 +80,38 @@ public class DriverScreenController {
     @FXML
     public void initialize() {
 
-        db = new DatabaseRepository();
-
         Stage primaryStage = PresentationLoader.getStage();
-
-        // Get pending orders
-
-        // Get delivered orders by this driver
 
         ArrayList<Object> price = new ArrayList<Object>();
         ArrayList<Object> cust = new ArrayList<Object>();
         ArrayList<Object> rest = new ArrayList<Object>();
         ArrayList<String> UnAcptId = new ArrayList<String>();
         ArrayList<Object> UnDelCost = new ArrayList<Object>();
-        ArrayList<String> Unaddr = new ArrayList<String>();
-        ArrayList<Object> Actid = new ArrayList<Object>();
-
-        /*
+        ArrayList<String> Unaddr= new ArrayList<String>();
+        ArrayList<Object> Actid=new ArrayList<Object>();
         ObjectId tempId;
+        DatabaseRepository.setup();
+        DatabaseRepository.getDB();
 
-        FindIterable<Document> pendingOrders = db.getOrders("pending");
-        FindIterable<Document> deliveredOrders = db.getOrders();
-
-        for (org.bson.Document doc : pendingOrders) {
+        FindIterable<org.bson.Document> ordPending = DatabaseRepository.getOrders("pending");
+        FindIterable<org.bson.Document> ordUnAccepted=DatabaseRepository.getOrders("UnAccepted");
+        for (org.bson.Document doc : ordPending) {
             price.add(doc.get("total_cost"));
             tempId = (ObjectId) doc.get("customer_id");
-            cust.add(db.getCust(tempId).get("email"));
+            cust.add(DatabaseRepository.getCust(tempId).get("email"));
             tempId = (ObjectId) doc.get("restaurant_id");
-            rest.add(db.getRest(tempId).get("name"));
-            tempId = (ObjectId) doc.get("_id");
+            rest.add(DatabaseRepository.getRest(tempId).get("name"));
+            tempId=(ObjectId) doc.get("_id");
             Actid.add(tempId);
         }
-
-        for (org.bson.Document doc : ordUnAccepted) {
+        
+        for (org.bson.Document doc : ordUnAccepted){
             UnAcptId.add(doc.get("_id").toString());
             tempId = (ObjectId) doc.get("customer_id");
-            Unaddr.add(db.getCust(tempId).get("address").toString());
+            Unaddr.add(DatabaseRepository.getCust(tempId).get("address").toString());
             UnDelCost.add(doc.get("delivery_cost"));
         }
-
+        
         custSize = cust.size();
         UnAcptIdSize = UnAcptId.size();
         Price = price;
@@ -116,30 +128,28 @@ public class DriverScreenController {
             btn.setOnAction(handleClick);
             orders_pane.getChildren().add(btn);
         }
-
-        for (int i = 0; i < Unaddr.size(); i++) {
-            Unaddr.set(i, Unaddr.get(i).replaceAll(",", " \n"));
+        
+        for (int i=0; i<Unaddr.size();i++){
+          Unaddr.set(i,Unaddr.get(i).replaceAll(",", " \n")); 
         }
         for (int i = 0; i < UnAcptIdSize; i++) {
             btn = new Button("Accept");
-            lab = new Label("Payment: €" + UnDelCost.get(i) + "\nAddress: " + Unaddr.get(i));
-            dashlab = new Label("--------------------");
+            lab= new Label("Payment: €"+UnDelCost.get(i)+"\nAddress: "+Unaddr.get(i));
+            dashlab=new Label("--------------------");
             btn.setLayoutY(i * 100);
             btn.setLayoutX(1);
             btn.setId("NewOr" + unacptid.get(i));
             btn.setOnAction(handleClick);
-            accept_pane.getChildren().addAll(lab, btn, dashlab);
+            accept_pane.getChildren().addAll(lab,btn,dashlab);
 
         }
 
-        */
-
-        // primaryStage.setTitle("Driver Screen");
+       // primaryStage.setTitle("Driver Screen");
         VBox OrderTab = new VBox();
         VBox AcceptTab = new VBox();
         VBox DetailsTab = new VBox();
         for (int i = 0; i < custSize; i++) {
-            btn = new Button("Complete: #" + (i + 1));
+            btn = new Button("Complete: #"+(i+1));
             btn.setLayoutY(i * 100);
             btn.setLayoutX(1);
             btn.setId("Order" + acptid.get(i));
@@ -156,48 +166,51 @@ public class DriverScreenController {
             AcceptTab.getChildren().add(btn);
 
         }
-
+        
         accept_pane.setPadding(new Insets(10, 20, 10, 5));
         DetailsTab.setPadding(new Insets(10, 20, 10, 5));
         orders_pane.setPadding(new Insets(10, 20, 10, 5));
         root.setPadding(new Insets(10, 10, 10, 5));
+        
+    TabPane DetailsTabPane=new TabPane();
 
-        TabPane DetailsTabPane = new TabPane();
-
-        for (int i = 0; i < Cust.size(); i++) {
-            VBox Tabv = new VBox();
-            Tabv.getChildren().addAll(new Label("Email: " + Cust.get(i)), new Label("Restaurant: " + Rest.get(i)),
-                    new Label("Price: €" + Price.get(i)));
-            Tab tab = new Tab("Order: #" + (i + 1), Tabv);
+        for(int i = 0; i<Cust.size(); i++){
+            VBox Tabv= new VBox();
+            Tabv.getChildren().addAll( new Label("Email: "+Cust.get(i)),new Label("Restaurant: "+Rest.get(i)),new Label("Price: €"+Price.get(i)));
+            Tab tab= new Tab("Order: #"+(i+1),Tabv);
             DetailsTabPane.getTabs().add(tab);
-        }
-
-        //DetailsPane.getChildren().add(DetailsTabPane);
     }
+        
+        DetailsPane.getChildren().add(DetailsTabPane);
+        
 
-    /*
-     * HBox check1 = new HBox(); cbx = new CheckBox(); cbx.setId("cbxAtRest");
-     * //cbx.setOnAction(checkHandler); lab = new Label();
-     * lab.setText("Parked At Restaurant"); check1.getChildren().addAll(cbx, lab);
-     * 
-     * HBox check2 = new HBox(); cbx = new CheckBox(); cbx.setId("cbxEnRoute");
-     * //cbx.setOnAction(checkHandler); lab = new Label(); lab.setText("Enroute");
-     * check2.getChildren().addAll(cbx, lab);
-     * 
-     * btn = new Button("Complete"); btn.setOnAction(handleClick);
-     * 
-     * DetailsTab.getChildren().addAll(check1, check2, btn);
-     * 
-     * //root.getChildren().addAll(orders_pane, DetailsTab, accept_pane);
-     * 
-     * if (id.substring(0, 5).equals("NewOr")) {
-     * System.out.println("Order Accepted: " + id.substring(8));
-     * 
-     * DatabaseRepository.acceptOrder(x); }
-     * 
-     * }
-     */
+        HBox check1 = new HBox();
+        cbx = new CheckBox();
+        cbx.setId("cbxAtRest");
+        //cbx.setOnAction(checkHandler);
+        lab = new Label();
+        lab.setText("Parked At Restaurant");
+        check1.getChildren().addAll(cbx, lab);
 
-    // primaryStage.show();
+        HBox check2 = new HBox();
+        cbx = new CheckBox();
+        cbx.setId("cbxEnRoute");
+        //cbx.setOnAction(checkHandler);
+        lab = new Label();
+        lab.setText("Enroute");
+        check2.getChildren().addAll(cbx, lab);
 
-}
+        btn = new Button("Complete");
+        btn.setOnAction(handleClick);
+
+        DetailsTab.getChildren().addAll(check1, check2, btn);
+
+        //root.getChildren().addAll(orders_pane, DetailsTab, accept_pane);
+
+       // Scene orderScene = new Scene(root, 1920, 1000);
+       // primaryStage.setScene(orderScene);
+
+        //primaryStage.show();
+    
+    }
+} 
